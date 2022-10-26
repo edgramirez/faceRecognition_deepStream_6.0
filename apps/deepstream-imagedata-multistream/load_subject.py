c834258f7ff3c59714778b935437648b0fa975ca
Edgar Cristobal Ramirez Miranda <edgar.ramirez@dormakaba.com>
2022-10-26 12:17:15 -0400

    Fixed - variables may not be defined

--- apps/deepstream-imagedata-multistream/load_subject.py
+++ apps/deepstream-imagedata-multistream/load_subject.py
@@ -7,7 +7,8 @@ import lib.common as com
 param_length = len(sys.argv)
 base_input_dir = com.BASE_INPUT_DB_DIRECTORY
 
-msg = 'Usage: ' + sys.argv[0] + ' newBlackList | newWhiteList | addToBlackList | addToWhiteList | removeBlackList | removeWhiteList '
+msg = 'Usage: ' + sys.argv[0] + ' newBlackList | newWhiteList | addToBlackList | addToWhiteList | removeBlackList | ' \
+                                'removeWhiteList '
 
 if param_length < 2:
     com.log_error(msg)
@@ -20,18 +21,18 @@ if sys.argv[1] == 'newBlackList' or sys.argv[1] == 'addToBlackList':
         com.create_data_dir(blacklist_face_images)
         try:
             blacklist_data = blacklist_results_dir + '/' + com.BLACKLIST_DB_NAME
+            import lib.biblioteca as biblio
+            if sys.argv[1] == 'newBlackList':
+                biblio.encode_known_faces_from_images_in_dir(blacklist_face_images, blacklist_data, 'blacklist')
+            else:
+                biblio.encode_known_faces_from_images_in_dir(blacklist_face_images, blacklist_data, 'blacklist', True)
         except AttributeError:
             com.log_error("Configuration error - environment variable 'BLACKLIST_DB_NAME' not set")
+        com.log_debug("Saving data in directory: {}".format(blacklist_results_dir))
+
     else:
         com.log_error(msg)
 
-    com.log_debug("Saving data in directory: {}".format(blacklist_results_dir))
-
-    import lib.biblioteca as biblio 
-    if sys.argv[1] == 'newBlackList':
-        biblio.encode_known_faces_from_images_in_dir(blacklist_face_images, blacklist_data, 'blacklist')
-    else:
-        biblio.encode_known_faces_from_images_in_dir(blacklist_face_images, blacklist_data, 'blacklist', True)
 elif sys.argv[1] == 'newWhiteList' or sys.argv[1] == 'addToWhiteList':
     if param_length == 2:
         whitelist_face_images = base_input_dir + '/whitelist_faces'
@@ -40,17 +41,17 @@ elif sys.argv[1] == 'newWhiteList' or sys.argv[1] == 'addToWhiteList':
         com.create_data_dir(whitelist_face_images)
         try:
             whitelist_data = whitelist_results_dir + '/' + com.WHITELIST_DB_NAME
+            import lib.biblioteca as biblio
+
+            if sys.argv[1] == 'newWhiteList':
+                biblio.encode_known_faces_from_images_in_dir(whitelist_face_images, whitelist_data, 'whitelist')
+            else:
+                biblio.encode_known_faces_from_images_in_dir(whitelist_face_images, whitelist_data, 'whitelist', True)
         except AttributeError:
             com.log_error("Configuration error - environment variable 'WHITELIST_DB_NAME' not set")
+        com.log_debug("Saving data in directory: {}".format(whitelist_results_dir))
+
     else:
         com.log_error(msg)
-
-    com.log_debug("Saving data in directory: {}".format(whitelist_results_dir))
-
-    import lib.biblioteca as biblio 
-    if sys.argv[1] == 'newWhiteList':
-        biblio.encode_known_faces_from_images_in_dir(whitelist_face_images, whitelist_data, 'whitelist')
-    else:
-        biblio.encode_known_faces_from_images_in_dir(whitelist_face_images, whitelist_data, 'whitelist', True)
 else:
     com.log_error(msg)
